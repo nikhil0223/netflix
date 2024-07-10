3e5aee827bd0c48525e8a80e464bb70d7d8bfbe8
Nikhil <94219647+nikhil0223@users.noreply.github.com>
2024-07-10 12:35:45 +0530

    comment

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const bodyParser = require('body-parser');
-const fetch = require('node-fetch'); // Add this line to require node-fetch
+const fetch = require('node-fetch');
 require('dotenv').config();
 const port = process.env.PORT;
 
